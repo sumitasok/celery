7074d0299e18f15e02d904db188fbe532ebc1c13
Sumit Asok <sumitasok@gmail.com>
2020-02-07 16:42:15 +0530

    backend postgres; json return not working;

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM python:3.8.1
 
 RUN pip install celery
+RUN pip install psycopg2
+RUN pip install sqlalchemy
 
 WORKDIR /app
--- tasks.py
+++ tasks.py
@@ -1,7 +1,20 @@
 from celery import Celery
 import os
 
-app = Celery('tasks', broker=os.environ['CELERY_CONN_STR'])
+app = Celery('tasks',
+    backend=os.environ['PG_CONN_STR'],
+    broker=os.environ['CELERY_CONN_STR'])
+
+app.conf.update(
+    task_serializer='json',
+    # accept_content=['json'],  # Ignore other content
+    # https://github.com/celery/celery/issues/3025
+    # Always using Pickle for Postgres; SQLAlchemy can be altered to serialise before pickling.
+    # should wait and watch if this issue is resolved; else have to consider another 
+    result_serializer='json',
+    timezone='Asia/Kolkata',
+    # enable_utc=True,
+)
 
 
 @app.task
